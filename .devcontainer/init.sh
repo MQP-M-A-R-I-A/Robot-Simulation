35551fe1e9b330a1f4d15e4de6394a2d39958191
Alex Tacescu <alextac98@gmail.com>
2021-03-24 11:16:45 -0400

    Modify display method to be more isolated and safer

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -13,14 +13,11 @@ printf "GROUP_ID=%s\n" "$GROUP_ID" >> $BUILD_ENV
 ##################################################
 ##### Set up x-server x-authentication files #####
 ##################################################
-# XSOCK=.devcontainer/.X11-unix
-# XAUTH=.devcontainer/.docker.xauth
-# XSOCK=/tmp/.X11-unix
-# XAUTH=/tmp/.docker.xauth
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
 
-# printf "XSOCK=%s\n" "$XSOCK" >> .devcontainer/xauth.env
-# printf "XAUTH=%s\n" "$XAUTH" >> .devcontainer/xauth.env
+printf "XSOCK=%s\n" "$XSOCK" >> $BUILD_ENV
+printf "XAUTH=%s\n" "$XAUTH" >> $BUILD_ENV
 
-# rm $XAUTH && touch $XAUTH
-# xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-# xhost +local:root
+rm $XAUTH && touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -

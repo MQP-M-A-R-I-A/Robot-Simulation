306a0a6179da61dd982a0d2d92571905aaf1872e
Alex Tacescu <alextac98@gmail.com>
2021-03-27 22:26:54 -0400

    Add namespace support, still non-working gazebo models

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -18,6 +18,7 @@ XAUTH=/tmp/.docker.xauth
 
 printf "XSOCK=%s\n" "$XSOCK" >> $BUILD_ENV
 printf "XAUTH=%s\n" "$XAUTH" >> $BUILD_ENV
+printf "XDG_RUNTIME=%s\n" "$XDG_RUNTIME_DIR" >> $BUILD_ENV
 
 rm $XAUTH && touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
